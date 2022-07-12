76c2398dc715a123ff566424cb14a6ed43456d90
bykimus-prime <alexander.rodzyanko@gmail.com>
2022-07-12 16:23:40 +0900

    Add mousedown 'hold to write' functionality but commented out

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const containerDiv = document.querySelector('#container');
+// let mouseDown = false; // this is for mousedown 'hold to write'
 
 function makeRows(rows, columns) {
     containerDiv.style.setProperty('--grid-rows', rows);
     containerDiv.style.setProperty('--grid-columns', columns);
-    console.log(containerDiv);
-    console.log(rows);
-    console.log(columns);
     for (i = 0; i < (rows * columns); i++) {
         let square = document.createElement('div');
         containerDiv.appendChild(square).className = 'grid-item';
@@ -16,3 +14,19 @@ function makeRows(rows, columns) {
 }
 
 makeRows(16, 16);
+
+// this is for mousedown 'hold to write'
+// document.addEventListener('mouseover', function (e) {
+//     if (mouseDown) {
+//         if (e.target.className === 'grid-item') {
+//             e.target.style.backgroundColor = 'black';
+//         }
+//     }
+// })
+
+// window.addEventListener('mousedown', () => {
+//     mouseDown = true;
+// })
+// window.addEventListener('mouseup', () => {
+//     mouseDown = false;
+// })
