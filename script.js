762d1adc74192664375db5bae3cc48a477345d30
bykimus-prime <alexander.rodzyanko@gmail.com>
2022-07-17 00:39:43 +0900

    Remove double borders from grid cells

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ function makeGrid(size) {
         square.addEventListener('mouseover', changeColor);
         square.addEventListener('mousedown', changeColor);
         grid.appendChild(square);
+        square.classList.add('border-top-left'); // add class to help prevent double borders
+    }
+
+    // add a right border to the right most items, prevent double borders
+    const rightItems = document.querySelectorAll(`.cell:nth-child(${size}n)`);
+    for (let i = 0; i < rightItems.length; i++) {
+        rightItems[i].setAttribute('data-right', 'true');
+        rightItems[i].classList.toggle('border-right');
+    }
+
+    // add a bottom border to the bottom most items, prevent double borders
+    let gridItems = document.querySelectorAll('.cell');
+    const lastItems = Array.from(gridItems).slice(-`${size}`);
+    for (let i = 0; i < lastItems.length; i++) {
+        lastItems[i].setAttribute('data-bottom', 'true');
+        lastItems[i].classList.toggle('border-bottom');
     }
 }
 
